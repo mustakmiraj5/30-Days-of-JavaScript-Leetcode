15733b3e3b5dc4cbf9ecc4cb3636b02f6ceb052e
mustakmiraj5 <msmiraj8@gmail.com>
2024-11-16 10:28:51 +0600

    Day-1

---
+++ helloWorldFunction.js
@@ -0,0 +1,26 @@
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+// const f = createHelloWorld();
+// console.log(f());
+
+// Editorial
+
+/**
+ * Closures and Higher order functions.
+ * Basic syntax, Anonymus function, Immediately Invoked Function Expression (IIFE).
+ * Functions Within Functions.
+ * Function Hoisting.
+ * Closures, lexical environment.
+ * Omit return.
+ * Rest Arguments.
+ */
